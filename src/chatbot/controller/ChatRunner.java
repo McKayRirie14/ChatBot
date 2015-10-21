ff27bf58900c1ab4991966e790a9a055d3a29e96
McKay Ririe <mrir2917@740s-pl-123.local>
2015-10-21 08:32:30 -0600

    Creating project and adding to repo.

---
+++ src/chatbot/controller/ChatRunner.java
@@ -0,0 +1,11 @@
+package chatbot.controller;
+
+public class ChatRunner
+{
+
+	public static void main(String[] args)
+	{
+		ChatController myChatController = new ChatControler();
+		myChatController.start();
+	}
+}
